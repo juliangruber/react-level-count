200a56b023fb7c886b184e03f983c16c0533df4f
Julian Gruber <julian@juliangruber.com>
2017-12-11 09:37:29 +0100

    fix missing value in .onput

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ export class Count extends React.Component {
     this.props.db.createReadStream({
       start: this.props.prefix,
       end: `${this.props.prefix}~`
-    }).on('data', ({ key }) => this.onput(key))
+    }).on('data', ({ key, value }) => this.onput(key, value))
     this.props.db.on('put', this.onput)
     this.props.db.on('del', this.ondel)
   }
